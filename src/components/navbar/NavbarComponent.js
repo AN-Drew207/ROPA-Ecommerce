b3cc944373d9108366a3ba9c1bbd0127326e66e2
AN-Drew <andrewgamerpro740@gmail.com>
2021-07-21 22:37:28 -0400

    1.11v

--- src/components/navbar/NavbarComponent.js
+++ src/components/navbar/NavbarComponent.js
@@ -7,7 +7,7 @@ const NavbarComponent = ()=>{
   <>
    <nav id="nav" className="navbar navbar-expand-sm navbar-light">
     <div className="container-fluid">
-     <a href="/"><img className="logo d-inline-block align-text-top " src={logo} alt="logo" /></a>
+     <a href="/ROPA-Ecommerce/"><img className="logo d-inline-block align-text-top " src={logo} alt="logo" /></a>
       <button className="navbar-toggler" onClick={()=>setDisplayList(!displayList)}>
       <i className="bi bi-list"></i>
     </button>
@@ -15,7 +15,7 @@ const NavbarComponent = ()=>{
     <div className="collapse navbar-collapse">
      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
       <li className="nav-item">
-       <a className="nav-link active" aria-current="page" href="/">Home</a>
+       <a className="nav-link active" aria-current="page" href="/ROPA-Ecommerce/">Home</a>
       </li>
       <li className="nav-item dropdown">
        <a className="nav-link dropdown-toggle active" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -46,10 +46,10 @@ const NavbarComponent = ()=>{
                   Categories
         </a>
         <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item" href="/Category/Electronics">Electronics</a></li>
-            <li><a className="dropdown-item" href="/Category/Jewelery">Jewelery</a></li>
-            <li><a className="dropdown-item" href="/Category/Men's Clothing">Men's Clothing</a></li>
-            <li><a className="dropdown-item" href="/Category/Women's Clothing">Women's Clothing</a></li>
+            <li><a className="dropdown-item" href="/ROPA-Ecommerce/Category/electronics">Electronics</a></li>
+            <li><a className="dropdown-item" href="/ROPA-Ecommerce/Category/jewelery">Jewelery</a></li>
+            <li><a className="dropdown-item" href="/ROPA-Ecommerce/Category/men's Clothing">Men's Clothing</a></li>
+            <li><a className="dropdown-item" href="/ROPA-Ecommerce/Category/women's Clothing">Women's Clothing</a></li>
         </ul>
        </li>
       <li className="nav-item px-2">
